0b06989ff88c2705b26fabac83870583d3ef452f
Amin <ameri.env@gmail.com>
2017-08-19 11:02:19 -0400

    New changes in config file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAABl3slnjIEBAEf0PLnZCFP1ONNZBy93XCGdoVnF218oPEV3MDZBd7L8QrptI1yK7ZAdap1oZAoc8GPnzlZAZAIXcaZB45wpoSTyrB9b7jG0A9pI8E1pkYFUTGytTrUaUZAaSit4w9dc0nmjJ1aZC5Jj0n69ja6x8NsxwsA8nx5S2evesxGfKfVBsQ',
-    FB_VERIFY_TOKEN: 'copper explain notice truck neat',
-    API_AI_CLIENT_ACCESS_TOKEN: '1bce24d160a44cd8b09d6e3903065ace',
-    FB_APP_SECRET: '908697120bd86acbbc067e5a016581c0',
-    SERVER_URL: 'https://slashui.herokuapp.com/webhook/',
+    FB_PAGE_TOKEN: 'EAABknnIqX80BAH7u4KRxYwat11Awd5m2ApOohCPOTHuQSkcABOKZAHwZCwjEVBNxjDoYrdr3q7B4f40QzSB12xUOl7ZCfZAmU8BYMe0PSifDq9C1f48htLnwCpqoEuJRBgGsYTZAI5lSfmVoXluqGQMcwcB0ePJUZAwIDrRHkqNAZDZD',
+    FB_VERIFY_TOKEN: 'testtest',
+    API_AI_CLIENT_ACCESS_TOKEN: '110631682990029',
+    FB_APP_SECRET: 'e79267b2207339903a43004f74410f1b',
+    SERVER_URL: 'https://obiwan-chat-bot.herokuapp.com/webhook/',
 };
