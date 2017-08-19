7dd21faadc958fb4ee99475a7d12ede5121a6d40
Amin <ameri.env@gmail.com>
2017-08-19 11:31:02 -0400

    weather

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
       // ];
       //sendQuickReply(sender, responseText, quick_replies);
       break;
-	case 'get-weather':	
+	case 'weather-action':	
       const options = {
         url: `http://api.apixu.com/v1/current.json?key=574239cfbb4e43c7a67122908171908&q=${parameters.city}`,
       };
